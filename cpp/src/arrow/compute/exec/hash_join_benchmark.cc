be2a55d4cbfdcb06b8b19ff9fe82f774124a8bbd
michalursa <michal@ursacomputing.com>
2022-02-03 00:27:13 -0800

    Faster version of hash join

--- cpp/src/arrow/compute/exec/hash_join_benchmark.cc
+++ cpp/src/arrow/compute/exec/hash_join_benchmark.cc
@@ -135,8 +135,9 @@ class JoinBenchmark {
 
     DCHECK_OK(join_->Init(
         ctx_.get(), settings.join_type, !is_parallel, settings.num_threads,
-        schema_mgr_.get(), {JoinKeyCmp::EQ}, std::move(filter), [](ExecBatch) {},
-        [](int64_t x) {}, schedule_callback));
+        &(schema_mgr_->proj_maps[0]), &(schema_mgr_->proj_maps[1]), {JoinKeyCmp::EQ},
+        std::move(filter), [](int64_t, ExecBatch) {}, [](int64_t x) {},
+        schedule_callback));
   }
 
   void RunJoin() {
