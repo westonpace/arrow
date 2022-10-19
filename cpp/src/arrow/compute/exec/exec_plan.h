bb78e34fe5b8089b7a123536befb436215839e02
Weston Pace <weston.pace@gmail.com>
2022-10-18 23:34:46 -0700

    ARROW-15732: Added default value to StartProducing to use CPU thread pool

--- cpp/src/arrow/compute/exec/exec_plan.h
+++ cpp/src/arrow/compute/exec/exec_plan.h
@@ -136,7 +136,8 @@ class ARROW_EXPORT ExecPlan : public std::enable_shared_from_this<ExecPlan> {
   ///
   /// Nodes are started in reverse topological order, such that any node
   /// is started before all of its inputs.
-  Status StartProducing(::arrow::internal::Executor* executor);
+  Status StartProducing(
+      ::arrow::internal::Executor* executor = ::arrow::internal::GetCpuThreadPool());
 
   /// \brief Stop producing on all nodes
   ///
