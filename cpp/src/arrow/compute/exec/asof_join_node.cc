6ec411ba2bdbefe1de3ace5d5b17b288ddd1072e
Weston Pace <weston.pace@gmail.com>
2022-11-02 13:44:33 -0700

    Adjust backpressure counter to start at 1.

--- cpp/src/arrow/compute/exec/asof_join_node.cc
+++ cpp/src/arrow/compute/exec/asof_join_node.cc
@@ -1342,7 +1342,7 @@ AsofJoinNode::AsofJoinNode(ExecPlan* plan, NodeVector inputs,
       must_hash_(must_hash),
       may_rehash_(may_rehash),
       tolerance_(tolerance),
-      backpressure_counter_(0),
+      backpressure_counter_(1),
       process_(),
       process_thread_(&AsofJoinNode::ProcessThreadWrapper, this) {
   finished_ = arrow::Future<>::MakeFinished();
